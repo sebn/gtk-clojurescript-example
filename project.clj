1c3c07f5d9103c8c5595b95f66627718da887fa3
Sébastien Nicouleaud <sebn@users.noreply.github.com>
2019-09-08 08:38:28 +0200

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject hello "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.89"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.520"]]
   :source-paths ["src"])
